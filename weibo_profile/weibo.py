623f3fbc0a2b3e2650930553cfe3f5f1192acf83
root <root@iZ23saac0yxZ.(none)>
2015-03-31 10:24:25 +0800

    add new weibo spider

--- weibo_profile/weibo.py
+++ weibo_profile/weibo.py
@@ -1,84 +1,106 @@
 # -*- coding: utf-8 -*-
 
-'''
-    新浪微博粉丝数和关注数抓取
-    输入参数：
-    微博用户id
-'''
-
 import urllib2
 import urllib
 import cookielib
-import sys 
+import sys
+from scrapy import Selector
 
 reload(sys)
-# 设置默认编码用于正确显示中文
 sys.setdefaultencoding('utf-8')
 
-'''
-    get_html_by_data函数
-    输入url和是否使用cookie标记use_cookie，返回该url对应的html源码
-    返回源码会存储在debug.html文件中，用于分析网页结构，可以将其注释
-'''
 def get_html_by_data(url, use_cookie=False):
-    # 初始化
     data = {}
     post_data = urllib.urlencode(data)
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     req = urllib2.Request(url)
     if use_cookie:
-        # 读取cookie
         cookie_file = open('cookie')
         cookie = cookie_file.read()
         req.add_header("Cookie", cookie)
-    # 添加user_agent头以模拟浏览器防止反抓取
     req.add_header("User-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36")
-    # 发送post请求
     f = opener.open(req)
-    # 获取html
     html = f.read()
-    # 写入debug.html文件中
     html_file = open('debug.html','w')
     print >> html_file, html
     f.close()
     return html
 
+def add(key, prod):
+    if prod.has_key(key):
+        return prod[key].replace('\r','').replace('\n','').replace(',',' ') + ','
+    return ','
 
-'''
-    work函数
-    获取微博的关注和粉丝数，并写入结果文件中
-    如果无法获取，请更新cookie并检查url是否正确
-'''
-def work(url):
-    try:
-        html = get_html_by_data(url, use_cookie=True)
-        # 利用split函数解析html
-        follow_cnt = html.split('关注[')[1].split(']')[0]
-        print u'关注数: ' + str(follow_cnt)
-        fans_cnt = html.split('粉丝[')[1].split(']')[0]
-        print u'粉丝数: ' + str(fans_cnt)
+def save_csv(prod):
+    file = open('xsh.csv','a')
+    resline = ""
+    resline += add('id', prod)
+    resline += add('date', prod)
+    resline += add('content', prod)
+    resline += add('quote', prod)
+    resline += add('comment', prod)
+    resline += add('like', prod)
+    print >> file, resline.encode('utf-8')
 
-        # 输出结果到文件result.txt
-        res_file = open('result.txt','w')
-        res_str = url + '\n' + 'follow_cnt: ' + str(follow_cnt) + '\n' + 'fans_cnt: ' + str(fans_cnt)
-        print >> res_file, res_str.encode('utf-8')
-    except Exception as e:
-        print e
-        print u'无法获取该页面上的关注和粉丝数！请更新cookie并检查输入id是否正确！'
-        exit(-1) 
+def work(id, page):
+    list_url = 'http://weibo.cn/' + id + '/profile?filter=1&page=' + str(page)
+    print list_url
+    hxs = Selector(text=get_html_by_data(list_url, use_cookie=True))
+    wb_list = hxs.xpath('//div[@class="c"]')
+    if len(wb_list) <= 3:
+        return
+    print 'Find ' + str(len(wb_list)) + ' weibo elements'
+    for wb in wb_list:
+        try:
+            prod = {}
+            prod['id'] = id
+            prod['content'] = ""
+            content_list = wb.xpath('.//span[@class="ctt"]/a/text()|.//span[@class="ctt"]/text()|.//span[@class="ctt"]/span/text()')
+            for elem in content_list:
+                prod['content'] += elem.extract()
+            print prod['content']
 
+            date = wb.xpath('.//span[@class="ct"]/text()')[0].extract()
+            prod['date'] = date.split(' ')[0]
+            if prod['date'].find('今天') != -1 or prod['date'].find('前') != -1:
+                prod['date'] = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+            elif prod['date'].find('月') != -1:
+                prod['date'] = '2015-' + prod['date'].replace('月','-').replace('日','')
+            print prod['date']
 
-'''
-    脚本程序入口
-'''
+            text_list = wb.xpath('.//a/text()')
+            for text in text_list:
+                text = text.extract()
+                if text.find('转发') != -1:
+                    eq1 = text.find('[')
+                    eq2 = text.find(']')
+                    prod['quote'] = text[eq1+1:eq2]
+                    print 'quote: ' + prod['quote']
+                if text.find('评论') != -1:
+                    eq1 = text.find('[')
+                    eq2 = text.find(']')
+                    prod['comment'] = text[eq1+1:eq2]
+                    print 'comment: ' + prod['comment']
+                if text.find('赞') != -1:
+                    eq1 = text.find('[')
+                    eq2 = text.find(']')
+                    prod['like'] = text[eq1+1:eq2]
+                    print 'like: ' + prod['like']
+            save_csv(prod)
+        except Exception as e:
+            print e
+            continue
+
+    work(id, page + 1)
+
 if __name__ == '__main__':
-    # 输入参数不合法
-    if len(sys.argv) != 2:
-        print 'python weibo.py weibo_id'
-        exit(-1)
-    # 获取输入id
-    id = sys.argv[1]
-    url = 'http://weibo.cn/u/' + id
-    work(url)
-    
+    id_list = []
+    with open('id.conf')  as id_file:
+       id_list = id_file.readlines()
+    for id in id_list:
+        try:
+            work(id.strip(), 1)
+        except Exception as e:
+            print e
+            continue
