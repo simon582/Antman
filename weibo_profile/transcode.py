d4ccdb09b602b845c5ba873a0d09e431dbfa5143
SimonShao <root@iZ23saac0yxZ.(none)>
2015-03-31 10:27:10 +0800

    add base weibo spider

---
+++ weibo_profile/transcode.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+for parent, dirnames, filenames in os.walk('result/'):
+    for filename in filenames:
+        print filename
+        if filename.find('csv') != -1:
+            with open('result/' + filename) as input_file, open('gbk/' + filename, 'w') as output_file:
+                lines = input_file.readlines()
+                for line in lines:
+                    print >> output_file, line.strip().encode('gbk', 'ignore')
